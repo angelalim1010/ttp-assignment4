c0a755b11f6baf2ad20d5825ac8868c9cc92e8e4
Mary Fan <maryfan@infires2.fios-router.home>
2019-05-31 22:04:40 -0400

    added working table to working funcs but not connected

--- setupFuncs.js
+++ setupFuncs.js
@@ -1,28 +1,4 @@
-let rowNum = 1;
-//alert(rowNum);
-let colNum = 1;
-//alert(colNum);
 let currentColor = "white";
-
-document.getElementById("addRow").addEventListener("click", function(event) {
-		rowNum += 1;
-		alert(rowNum);
-});
-document.getElementById("subRow").addEventListener("click", function(event) {
-		rowNum -= 1;
-		alert(rowNum);
-});
-
-
-document.getElementById("addCol").addEventListener("click", function(event) {
-		colNum += 1;
-		alert(colNum);
-});
-document.getElementById("subCol").addEventListener("click", function(event) {
-		colNum -= 1;
-		alert(colNum);
-});
-
 
 document.getElementById("selectColor").addEventListener("change", function(event) {
 	if (document.getElementById("selectColor").value === "WHITE") {
@@ -67,9 +43,45 @@ document.getElementById("clickHold").addEventListener("click", function(event) {
 
 
 
+function createCell(cell, style) {
+    let div = document.createElement('div') // create DIV element
+    div.setAttribute('class', style);        // set DIV class attribute
+    div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+    cell.appendChild(div);                   // append DIV to the table cell
+}
 
+function addRow() {
+    let tbl = document.getElementById('myTable'), // table reference
+          row = tbl.insertRow(tbl.rows.length),      // append table row
+          i;
+      // insert table cells to the new row
+      for (i = 0; i < tbl.rows[0].cells.length; i++) {
+          createCell(row.insertCell(i), 'row');
+      }
+}
 
+function deleteRow(){
+    document.getElementById("myTable").deleteRow(1);
+}
 
+function addColumn(){
+    // append column to the HTML table
+    let tbl = document.getElementById('myTable'), // table reference
+        i;
+    // open loop for each row and append cell
+    for (i = 0; i < tbl.rows.length; i++) {
+        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
+    }
+}
 
-
-
+function deleteColumn(){
+    let allRows = document.getElementById('myTable').rows;
+ for (let i=0; i< allRows.length; i++) {
+  if (allRows[i].cells.length > 1) {
+   allRows[i].deleteCell(-1); //delete the cell
+  } else {
+   console.log("You can't delete more columns.");
+   return;
+  }
+ }
+}
