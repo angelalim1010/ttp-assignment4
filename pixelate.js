170c3d2108a9896b607918d8f404e3de1def8eb9
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-01 10:07:41 -0400

    adding setcolo

--- pixelate.js
+++ pixelate.js
@@ -1,21 +1,24 @@
 function createCell(cell, style) {
     let div = document.createElement('div') // create DIV element
-    div.setAttribute('class', style);        // set DIV class attribute
-    div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+    //div.setAttribute('class', style);        // set DIV class attribute
+    //div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
     cell.appendChild(div);                   // append DIV to the table cell
-    div.id = "testPixel";
 }
 
 function addRow() {
     let tbl = document.getElementById('myTable'), // table reference
           row = tbl.insertRow(tbl.rows.length),      // append table row
           i;
+      // if(tbl.rows.length > 12){
+      //     return;
+      // }
       // insert table cells to the new row
       for (i = 0; i < tbl.rows[0].cells.length; i++) {
-          createCell(row.insertCell(i), 'row');
+              createCell(row.insertCell(i), 'row');
       }
-      console.log(document.getElementById("testPixel"));
-}
+      console.log("row created" + document.getElementById("testPixel"));
+
+  }
 
 function deleteRow(){
     document.getElementById("myTable").deleteRow(1);
@@ -25,10 +28,12 @@ function addColumn(){
     // append column to the HTML table
     let tbl = document.getElementById('myTable'), // table reference
         i;
+
     // open loop for each row and append cell
-    for (i = 0; i < tbl.rows.length; i++) {
-        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
-    }
+        for (i = 0; i < tbl.rows.length; i++) {
+            createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), 'col');
+            console.log("column made" + document.getElementById("testPixel"));
+        }
 }
 
 function deleteColumn(){
@@ -41,4 +46,9 @@ function deleteColumn(){
    return;
   }
  }
+}
+function setColor(){
+
+        document.getElementByTag("div").style.background="black";
+
 }
