b21772b84e657ca6d3f7d4415e731af902d8d200
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-01 21:57:28 -0400

    adding setcolor method

--- pixelate.js
+++ pixelate.js
@@ -24,12 +24,46 @@ function addColumn(){
 
 function deleteColumn(){
     let allRows = document.getElementById('myTable').rows;
- for (let i=0; i< allRows.length; i++) {
-  if (allRows[i].cells.length > 1) {
-   allRows[i].deleteCell(-1); //delete the cell
-  } else {
-   console.log("You can't delete more columns.");
-   return;
-  }
- }
+     for (let i=0; i< allRows.length; i++) {
+          if (allRows[i].cells.length > 1) {
+              allRows[i].deleteCell(-1); //delete the cell
+         }
+         else {
+              console.log("You can't delete more columns.");
+           return;
+         }
+    }
 }
+
+function setColor(obj,color){
+    obj.style.backgroundColor = color;
+}
+
+function changeColor(){
+    let isDrawing = false;
+    const cells = document.getElementsByTagName("td");
+    for(let cell of cells){
+
+    cell.addEventListener('mousedown',event => {
+        isDrawing = true;
+        console.log(cell);
+        console.log(cells.length);
+        setColor(cell,"Purple");
+    });
+
+    cell.addEventListener('mousemove',event => {
+        if(isDrawing){
+            setColor(cell,"Purple");
+        }
+    });
+
+    cell.addEventListener('mouseup',event => {
+        if(isDrawing){
+            isDrawing = false;
+        }
+    });
+}
+
+}
+
+changeColor();
